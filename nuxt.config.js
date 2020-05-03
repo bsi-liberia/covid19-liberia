68975e0118ce53750954c2eba43b714f007b47cc
Mark Brough <mark.brough@gmail.com>
2020-05-03 20:47:21 +0200

    Change name to property in social media tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,12 +20,12 @@ export default {
       { name: 'twitter:description', content: "A dashboard of revenue and expenditure data for the COVID-19 response in Liberia." },
       { name: 'twitter:image', content: 'https://covidresponseliberia.org/visualisation.png' },
       { name: 'twitter:image:alt', content: 'Revenue for the COVID-19 response.' },
-      { name: 'og:url', content: 'https://covidresponseliberia.org' },
-      { name: 'og:title', content: 'COVID-19 Response Liberia' },
-      { name: 'og:type', content: 'website' },
-      { name: 'og:description', content: "A dashboard of revenue and expenditure data for the COVID-19 response in Liberia." },
-      { name: 'og:image', content: 'https://covidresponseliberia.org/visualisation.png' },
-      { name: 'og:image:alt', content: 'Revenue for the COVID-19 response.' },
+      { property: 'og:url', content: 'https://covidresponseliberia.org' },
+      { property: 'og:title', content: 'COVID-19 Response Liberia' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:description', content: "A dashboard of revenue and expenditure data for the COVID-19 response in Liberia." },
+      { property: 'og:image', content: 'https://covidresponseliberia.org/visualisation.png' },
+      { property: 'og:image:alt', content: 'Revenue for the COVID-19 response.' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
