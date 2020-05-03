c15d4d0cdef33463fdcf5385dcfe054b51e740ef
Mark Brough <mark.brough@gmail.com>
2020-05-03 20:43:46 +0200

    Add facebook social media tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,13 +13,19 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'A visualisation of the COVID-19 response in Liberia.' },
+      { hid: 'description', name: 'description', content: 'A dashboard of revenue and expenditure data for the COVID-19 response in Liberia.' },
       { name: 'twitter:card', content: 'summary' },
       { name: 'twitter:site', content: '@mfdp_lib' },
       { name: 'twitter:title', content: 'COVID-19 Response Liberia' },
-      { name: 'twitter:description', content: "A visualisation of the COVID-19 response in Liberia." },
-      { name: 'twitter:image', content: 'http://covidresponseliberia.org/visualisation.png' },
-      { name: 'twitter:image:alt', content: 'Revenue for the COVID-19 response.' }
+      { name: 'twitter:description', content: "A dashboard of revenue and expenditure data for the COVID-19 response in Liberia." },
+      { name: 'twitter:image', content: 'https://covidresponseliberia.org/visualisation.png' },
+      { name: 'twitter:image:alt', content: 'Revenue for the COVID-19 response.' },
+      { name: 'og:url', content: 'https://covidresponseliberia.org' },
+      { name: 'og:title', content: 'COVID-19 Response Liberia' },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:description', content: "A dashboard of revenue and expenditure data for the COVID-19 response in Liberia." },
+      { name: 'og:image', content: 'https://covidresponseliberia.org/visualisation.png' },
+      { name: 'og:image:alt', content: 'Revenue for the COVID-19 response.' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
