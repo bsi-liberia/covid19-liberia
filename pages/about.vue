4619b9486929be95fff9885e3b2de10a9aeac6db
Mark Brough <mark.brough@gmail.com>
2020-06-07 01:20:38 +0200

    Fix typo

--- pages/about.vue
+++ pages/about.vue
@@ -7,8 +7,8 @@
       <ul>
         <li><b>Cases</b> &ndash; data on cases and deaths over time, according to data provided by the Ministry of Health and National Public Health Institute.</li>
         <li><b>Revenue</b> &ndash; data on total receipts from the Government of Liberia, donor partners including the World Bank,  businesses and individual contributors.</li>
-        <li><b>Expenditure</b> &ndash; data on variuos expenditures including case management, surveillance, lab operations, risk and communication, support to county health team, etc.</li>
-        <li><b>In-Kind Contributions</b> &ndash; data on in-kind rather than cash contributions from private businesses, non-governmental organizations, and ordinary citizens. This donations are  then converted to a monetary value using market prices to aid comparability.</li>
+        <li><b>Expenditure</b> &ndash; data on various expenditures including case management, surveillance, lab operations, risk and communication, support to county health team, etc.</li>
+        <li><b>In-Kind Contributions</b> &ndash; data on in-kind rather than cash contributions from private businesses, non-governmental organizations, and ordinary citizens. This donations are then converted to a monetary value using market prices to aid comparability.</li>
       </ul>
       <p>On the revenue, expenditure, and in-kind contributions pages, users can filter and adjust the display of the data, by clicking the grey "chart options" button below the chart.</p>
       <p>** All data for the dashboard are provided by the Incident Management Team & and the National Response Center, comprising of the Ministry of Health, National Public Health Institute, and the Ministry of Finance and Development Planning.</p>
