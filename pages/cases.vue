1858a1c3573479250b8db4b99c535cf7cfd7f2da
Mark Brough <mark.brough@gmail.com>
2020-06-06 19:34:32 +0200

    Move cases data to its own tab

---
+++ pages/cases.vue
@@ -0,0 +1,145 @@
+<template>
+  <div>
+    <b-container>
+      <h2>Cases</h2>
+      <template v-if="cases.length==0">
+        <div class="text-center text-secondary">
+          <hr />
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Loading...</strong>
+          <hr />
+        </div>
+      </template>
+      <template v-else>
+        <h3>Total cases over time</h3>
+        <b-badge class="last-updated" variant="light" pill>as of {{ lastUpdatedCases }}</b-badge>
+        <BarLineChart :barChartData="cases"
+        labelField="Date"
+        valueLabel="Number of cases"
+        valueField="Cases"
+        valuePrecision="0"
+        chartType="bar"
+        :commitments="false"
+        :maximumValues="100"
+        colour="#1f77b4" />
+        <hr />
+        <h3>By county, to date</h3>
+        <b-badge class="last-updated" variant="light" pill>as of {{ lastUpdatedCases }}</b-badge>
+        <BarChart :barChartData="counties"
+        labelField="County"
+        valueLabel="Number of cases"
+        valueField="Cases"
+        valuePrecision="0"
+        chartType="bar"
+        :commitments="false"
+        :maximumValues="100" />
+      </template>
+    </b-container>
+  </div>
+</template>
+<style scoped>
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 3.5rem;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 2rem;
+  word-spacing: 5px;
+  /* padding-bottom: 15px; */
+  max-width: 800px;
+}
+
+</style>
+<script>
+import config from '../nuxt.config'
+import BarChart from '~/components/BarChart.vue'
+import BarLineChart from '~/components/BarLineChart.vue'
+
+export default {
+  components: {
+    BarChart,
+    BarLineChart
+  },
+  data() {
+    return {
+      title: config.head.title
+    }
+  },
+  computed: {
+    cases() {
+      return this.$store.state.casesData
+    },
+    counties() {
+      return this.$store.state.countiesData
+    },
+    lastUpdated() {
+      return this.$store.state.lastUpdated
+    },
+    lastUpdatedCases() {
+      return this.$store.state.lastUpdatedCases
+    },
+    revenueData() {
+      return this.$store.state.revenueData
+    },
+    expenditureData() {
+      return this.$store.state.expenditureData
+    },
+    inKindData() {
+      return this.$store.state.inKindData
+    },
+    committed() {
+      return this.revenueData.reduce((total, item) => {
+        total += item.Commitment
+        return total
+      }, 0.00)
+    },
+    disbursed() {
+      return this.revenueData.reduce((total, item) => {
+        total += item.Disbursement
+        return total
+      }, 0.00)
+    },
+    spent() {
+      return this.expenditureData.reduce((total, item) => {
+        total += item.Disbursement
+        return total
+      }, 0.00)
+    }
+  },
+  methods: {
+    makeNumber(value) {
+      const _value = parseFloat(value.replace(/,/g, ""))
+      if (isNaN(_value)) { return 0.00 }
+      return _value
+    },
+    numberFormatter(value) {
+      return value ? value.toLocaleString(undefined, {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2
+      }) : ""
+    },
+    async loadData() {
+      await this.$store.dispatch('getCasesData')
+      await this.$store.dispatch('getCountiesData')
+      this.$nuxt.$loading.finish()
+    }
+  },
+  watch: {
+  },
+  mounted() {
+    this.$nextTick(() => {
+      if (this.cases.length == 0) {
+        this.$nuxt.$loading.start()
+        this.loadData()
+      }
+    })
+  }
+}
+</script>
