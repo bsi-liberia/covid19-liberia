6e6ad3ca34c517320e30ef55966e096715633355
Mark Brough <mark.brough@gmail.com>
2020-06-09 21:59:08 +0200

    Add social media buttons to in kind

--- pages/in-kind.vue
+++ pages/in-kind.vue
@@ -7,6 +7,8 @@
         </b-col>
         <b-col md="4" class="text-md-right">
           <b-badge class="last-updated" variant="light">as of {{ lastUpdated }}</b-badge>
+          <br />
+          <SocialMedia url="http://covid19response.gov.lr/in-kind/" />
         </b-col>
       </b-row>
       <template v-if="inKindData.length==0">
@@ -91,10 +93,12 @@
 <script>
 import config from '../nuxt.config'
 import BarChart from '~/components/BarChart.vue'
+import SocialMedia from '~/components/SocialMedia.vue'
 
 export default {
   components: {
-    BarChart
+    BarChart,
+    SocialMedia
   },
   data() {
     return {
