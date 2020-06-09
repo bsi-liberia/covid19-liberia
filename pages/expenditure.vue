721b83f1893e3d0842234c1124324a3e16340423
Mark Brough <mark.brough@gmail.com>
2020-06-09 21:58:58 +0200

    Add social media buttons to expenditure

--- pages/expenditure.vue
+++ pages/expenditure.vue
@@ -7,6 +7,8 @@
         </b-col>
         <b-col md="4" class="text-md-right">
           <b-badge class="last-updated" variant="light">as of {{ lastUpdated }}</b-badge>
+          <br />
+          <SocialMedia url="http://covid19response.gov.lr/expenditure/" />
         </b-col>
       </b-row>
       <template v-if="expenditureData.length==0">
@@ -105,10 +107,12 @@
 <script>
 import config from '../nuxt.config'
 import BarChart from '~/components/BarChart.vue'
+import SocialMedia from '~/components/SocialMedia.vue'
 
 export default {
   components: {
-    BarChart
+    BarChart,
+    SocialMedia
   },
   data() {
     return {
